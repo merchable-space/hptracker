086d76944618240c1dbb66669931faf0c3f42044
digitalAlchemist <webmaster@merchable.space>
2018-02-28 20:52:15 +0000

    WIP

--- app/main/controllers/track-ctrl.js
+++ app/main/controllers/track-ctrl.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('main')
-.controller('TrackCtrl', function ($log, $http, $timeout, Main, Config, $cordovaDevice) {
+.controller('TrackCtrl', function () {
 
   var trackerVm = this;
 
@@ -9,15 +9,37 @@ angular.module('main')
 
   trackerVm.addAlly = addAlly;
   trackerVm.addEnemy = addEnemy;
+  trackerVm.swapStatus = swapStatus;
+  trackerVm.deleteMe = deleteMe;
+  trackerVm.addHealth = addHealth;
+  trackerVm.remHealth = remHealth;
 
   return trackerVm;
 
   function addAlly () {
     var thisAlly = {
+      deleted: false,
       name: '',
       hp: 1,
       maxhp: 1,
-      status: {}
+      changeHpValue: 1,
+      status: {
+        'blind': false,
+        'charm': false,
+        'deaf': false,
+        'fatigue': false,
+        'grapple': false,
+        'incap': false,
+        'invis': false,
+        'paralyze': false,
+        'petrify': false,
+        'poison': false,
+        'prone': false,
+        'restrain': false,
+        'stun': false,
+        'unconcious': false,
+      },
+      'hover': '',
     };
 
     trackerVm.allies.push(thisAlly);
@@ -25,13 +47,55 @@ angular.module('main')
 
   function addEnemy () {
     var thisEnemy = {
+      deleted: false,
       name: '',
       hp: 1,
       maxhp: 1,
-      status: {}
+      changeHpValue: 1,
+      status: {
+        'blind': false,
+        'charm': false,
+        'deaf': false,
+        'fatigue': false,
+        'grapple': false,
+        'incap': false,
+        'invis': false,
+        'paralyze': false,
+        'petrify': false,
+        'poison': false,
+        'prone': false,
+        'restrain': false,
+        'stun': false,
+        'unconcious': false,
+      },
+      'hover': '',
     };
 
     trackerVm.enemies.push(thisEnemy);
+  }
+
+  function swapStatus (object, stat) {
+    if (object.status[stat] === true) {
+      object.status[stat] = false;
+    } else {
+      object.status[stat] = true;
+    }
+  }
+
+  function addHealth (object) {
+    object.hp += parseInt(object.changeHpValue);
+  }
+
+  function remHealth (object) {
+    if (object.hp <= 0) {
+      return false;
+    }
+
+    object.hp -= parseInt(object.changeHpValue);
+  }
+
+  function deleteMe (object) {
+    object.deleted = true;
   }
 
 });
